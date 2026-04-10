e987a9b3dca30a7d8791b337987577a3f38f2a9c
hubotao <lnfromnorth@gmail.com>
2026-04-10 17:11:33 +0800

    update: finish 06, 07

--- exercises/06_stack_maze/06_stack_maze.c
+++ exercises/06_stack_maze/06_stack_maze.c
@@ -11,10 +11,79 @@ int maze[MAX_ROW][MAX_COL] = {
 	0, 0, 0, 1, 0,
 };
 
+int visited[MAX_ROW][MAX_COL] = {0};
+
+// map move control
+struct move {
+	int dx;
+	int dy;
+} move_list[4] = {
+	{0, -1}, {-1, 0}, {0, 1}, {1, 0}
+};
+
+int check_next_step(int x, int y) {
+	if(x >= 0 && x < MAX_COL && y >= 0 && y < MAX_ROW)
+		if(visited[y][x] != 1 && maze[y][x] != 1)
+			return 1;
+	return 0;
+}
+
+// stack
+struct pos {
+	int x;
+	int y;
+	int step;
+};
+
+struct pos pos_stack[MAX_COL * MAX_ROW] = {};
+int stop = 0;
+
+void stack_push(int x, int y, int step) {
+	pos_stack[stop].x = x;
+	pos_stack[stop].y = y;
+	pos_stack[stop].step = step;
+	stop++;
+}
+
+void stack_pop() {
+	stop--;
+}
+
+struct pos stack_top() {
+	return pos_stack[stop - 1];
+}
+
+void stack_update_step(int step) {
+	pos_stack[stop - 1].step = step;
+}
+
 int main(void)
 {
 	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+	stack_push(0, 0, -1);
+	visited[0][0] = 1;
+
+	while(1) {
+		struct pos curr = stack_top();
+		if(curr.x == 4 && curr.y == 4)
+			break;
+		int i = curr.step + 1;
+		for(; i < 4; i++) {
+			int x = curr.x + move_list[i].dx;
+			int y = curr.y + move_list[i].dy;
+			if(check_next_step(x, y)) {
+				stack_update_step(i);
+				visited[y][x] = 1;
+				stack_push(x, y, -1);
+				break;
+			}
+		}
+		if(i == 4) stack_pop();
+	}
+
+	for(int i = stop - 1; i >= 0; i--) {
+		printf("(%d,%d)\n", pos_stack[i].y, pos_stack[i].x);
+	}
 
 	return 0;
 }
--- exercises/07_queue_maze/07_queue_maze.c
+++ exercises/07_queue_maze/07_queue_maze.c
@@ -11,10 +11,102 @@ int maze[MAX_ROW][MAX_COL] = {
 	0, 0, 0, 1, 0,
 };
 
+int visited[MAX_ROW][MAX_COL] = {0};
+
+// map move control
+struct move {
+	int dx;
+	int dy;
+} move_list[4] = {
+	{0, -1}, {-1, 0}, {0, 1}, {1, 0}
+};
+
+int check_next_step(int x, int y) {
+	if(x >= 0 && x < MAX_COL && y >= 0 && y < MAX_ROW)
+		if(visited[y][x] != 1 && maze[y][x] != 1)
+			return 1;
+	return 0;
+}
+
+// node type
+typedef struct {
+	int x;
+	int y;
+	int path;
+} node;
+
+// queue
+#define QUEUE_DEPTH 10
+
+node queue[QUEUE_DEPTH];
+int head = 0, tail = 0;
+
+int queue_push(node nd) {
+	// queue full
+	if( tail + 1 == head || (head == 0 && tail == (QUEUE_DEPTH -1))) return 0;
+	else {
+		queue[tail] = nd;
+		tail = (tail + 1) % QUEUE_DEPTH;
+	}
+}
+
+int queue_pop() {
+	if(head == tail) return 0;
+	else {
+		head = (head + 1) % QUEUE_DEPTH;
+		return 1;
+	}
+}
+
+node queue_head() {
+	return queue[head];
+}
+
+int queue_size() {
+	return (tail > head) ? (tail - head) : (tail - head + 10);
+}
+
+
 int main(void)
 {
 	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+	// record path
+	node apath[MAX_COL * MAX_ROW];
+	int index = 0;
+	// initial node
+	node start = {0, 0, -1};
+	queue_push(start);
+	visited[0][0] = 0;
+	while(1) {
+		node current = queue_head();
+		apath[index++] = current;
+		queue_pop();
+		// reach end
+		if(current.x == 4 && current.y == 4)
+			break;
+		for(int i = 0; i < 4; i++) {
+			int x = current.x + move_list[i].dx;
+			int y = current.y + move_list[i].dy;
+			if(check_next_step(current.x + move_list[i].dx, current.y + move_list[i].dy)) {
+				node append = {x, y, index - 1};
+				visited[y][x] = 1;
+				queue_push(append);
+			}
+		}
+	}
 
+	int next = index - 1;
+	node output_list[100];
+	int output_idx = 0;
+	while(1) {
+		// printf("(%d,%d) ", apath[next].y, apath[next].x);
+		output_list[output_idx++] = apath[next];
+		if(apath[next].path == -1) break;
+		next = apath[next].path;
+	}
+
+	for(int i = 0; i < output_idx; i++) {
+		printf("(%d,%d)\n", output_list[i].y, output_list[i].x);
+	}
 	return 0;
 }
